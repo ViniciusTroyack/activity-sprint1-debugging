b966120e77609290747748cfc74c0eb3088b2334
Vinicius <troyackvinicius@gmail.com>
2021-07-14 23:34:34 -0300

    primeiro commit da atividade

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import CardsList from "./components/cards-list";
-import useEffect from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export const App = () => {
@@ -10,34 +10,37 @@ export const App = () => {
   const handleDeckRequest = () => {
     fetch("https://deckofcardsapi.com/api/deck/new/")
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((res) => setDeck(res.deck_id));
   };
 
   const handleCardsRequest = (deckId) => {
-    fetch("https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52")
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
       .then((res) => res.json())
-      .then((res) => setCardsList([...res.cards]));
+      .then((res) => setCardsList(res.cards));
   };
 
   const handleShowDeck = () => {
-    setShowDeck(false);
+    setShowDeck(true);
   };
 
   useEffect(() => {
     handleDeckRequest();
-  }, [deck]);
+  }, []);
 
   useEffect(() => {
-    if (deck) handleCardsRequest(deck);
-  }, []);
+    handleCardsRequest(deck);
+  }, [deck]);
+
 
   return (
     <div className="main-container">
       <h1 className="main-title">Debugue para ver o baralho</h1>
-      <button onChange={handleShowDeck} className="new-deck-button">
+      <button onClick={() => handleShowDeck()} className="new-deck-button">
         Novo baralho
       </button>
-      {ShowDeck & <CardsList cardsList={cardsList} />}
+      {showDeck && <CardsList cardsList={cardsList} />}
     </div>
   );
 };
+
+export default App;
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Card = (card) => {
+const Card = ({ card }) => {
   return <img src={card.image} alt={card.code} className="card" />;
 };
 
